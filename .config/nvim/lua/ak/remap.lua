fcf99e6e9423c10f6c6647dafb1bb6d03400d8c6
Akhtam <akhtamismatov@akm3pro.local>
2025-01-21 00:39:53 -0800

    up

--- .config/nvim/lua/ak/remap.lua
+++ .config/nvim/lua/ak/remap.lua
@@ -4,7 +4,7 @@ local keymap = vim.keymap
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 --window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
@@ -18,14 +18,3 @@ keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 keymap.set("n", "<leader>nr", ":set relativenumber!<CR>", { desc = "Toggle relative number" })
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
--- .config/nvim/lua/ak/set.lua
+++ .config/nvim/lua/ak/set.lua
@@ -37,3 +37,14 @@ opt.smartindent = true
 
 -- turn off swapfile
 opt.swapfile = false
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
