a3a2af9acd5eb35b3241069d5734164206f31a88
Akhtam <akhtamismatov@akm3pro.local>
2025-01-26 00:01:30 -0800

    updates

--- .config/nvim/lua/ak/lazy/nvim_tmux.lua
+++ .config/nvim/lua/ak/lazy/nvim_tmux.lua
@@ -2,10 +2,9 @@ return {
 	"alexghergh/nvim-tmux-navigation",
 	config = function()
 		-- local nvim_tmux_nav = require("nvim-tmux-navigation")
-		require("nvim-tmux-navigation").setup({})
-		-- nvim_tmux_nav.setup({
-		-- 	disable_when_zoomed = true, -- defaults to false
-		-- })
+		require("nvim-tmux-navigation").setup({
+			disable_when_zoomed = true, -- defaults to false
+		})
 		--
 		-- vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
 		-- vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
